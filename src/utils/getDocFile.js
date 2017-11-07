defdb691f9db81a409fc84110ee3ea87f9c2f47d
Rafael Escala Neyra <rescala@profuturo.afp>
2017-11-07 17:17:42 -0500

    Added jsdoc config

--- src/utils/getDocFile.js
+++ src/utils/getDocFile.js
@@ -1,11 +1,13 @@
 const jsdoc = require('jsdoc-api');
 const getParseBabel = require('./getParseBabel');
+const path = require('path');
 
 export default function getDocFile (jscodeReqest, file) {
 	try {
 		const babelifycode = getParseBabel(jscodeReqest, '2017', true);
 		let docReturn = jsdoc.explainSync({
 				source: babelifycode.code,
+				configure: path.join(path.dirname(__dirname), '..', 'config.json'),
 		}).filter(obj => obj.undocumented !== true)
 			.map( obj => {
 				if ( obj.meta ) {
