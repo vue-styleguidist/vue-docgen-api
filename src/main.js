51a821e438a739d3cadc43237a08063406acf504
Mo <Radiergummi@users.noreply.github.com>
2017-11-27 12:26:05 +0100

    Added parseSource

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import * as utils from './utils';
-import parse from './parse';
+import { parseFile, parseSource } from './parse';
 
-function defaultParse(src) {
-  return parse(src);
+function parseFile(path) {
+  return parseFile(path);
+}
+
+function parseSource(src) {
+	return parseSource(src);
 }
 
 export {
-	defaultParse as parse,
+	parseFile,
+	parseSource,
 	utils,
 };
 
