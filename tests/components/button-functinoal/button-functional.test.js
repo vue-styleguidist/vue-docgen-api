e1ef21421897d17dd12727d7bbda0f77348c87f0
Bart Ledoux <ledouxb@me.com>
2018-12-01 23:10:11 -0600

    add functional component test

---
+++ tests/components/button-functinoal/button-functional.test.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const api = require('../../../src/main')
+const button = path.join(__dirname, './MyButton.vue')
+let docButton
+
+describe('tests button', () => {
+  beforeEach(function(done) {
+    docButton = api.parse(button)
+    done()
+  })
+
+  it('should have a slot.', () => {
+    expect(Object.keys(docButton['slots']).length).toEqual(1)
+  })
+
+  it('should have a default slot.', () => {
+    expect(typeof docButton['slots']['default'] !== 'undefined').toBe(true)
+  })
+
+  it('the default slot should have "Use this slot default" as description', () => {
+    expect(docButton['slots']['default']['description']).toEqual('Use this slot default')
+  })
+
+  it('should the component has size prop description equal The size of the button', () => {
+    expect(docButton['props']['size']['description']).toEqual('The size of the button')
+  })
+})
