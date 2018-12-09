fbdb9b0b9f900074920315f04f3eabebbc47f44e
Bart Ledoux <ledouxb@me.com>
2018-12-09 15:12:09 -0600

    whitespaces

--- src/utils/processMethods.js
+++ src/utils/processMethods.js
@@ -1,34 +1,34 @@
 import getMethod from './getMethod'
 
 export default function processMethods(docFile, component) {
-  docFile = docFile.slice()
-  let methods = component.methods
-  const listDocMethods = []
-  let mixins = component.mixins
-  if (mixins) {
-    mixins.forEach(mixin => {
-      const mMixin = mixin.methods
-      if (mMixin) {
-        methods = Object.assign({}, mMixin, methods)
-      }
-    })
-  }
-  if (methods) {
-    const listDocParts = []
-    Object.keys(methods).forEach(methodName => {
-      const docPart = docFile.reverse().filter(comment => {
-        return (
-          comment.longname.indexOf('methods.' + methodName) > -1 &&
-          listDocParts.indexOf(comment.longname) === -1
-        )
-      })[0]
-      if (docPart) {
-        if (docPart['access'] && docPart['access'] === 'public') {
-          listDocParts.push(docPart.longname)
-          listDocMethods.push(getMethod(methodName, docPart))
-        }
-      }
-    })
-  }
-  return listDocMethods
+	docFile = docFile.slice()
+	let methods = component.methods
+	const listDocMethods = []
+	let mixins = component.mixins
+	if (mixins) {
+		mixins.forEach(mixin => {
+			const mMixin = mixin.methods
+			if (mMixin) {
+				methods = Object.assign({}, mMixin, methods)
+			}
+		})
+	}
+	if (methods) {
+		const listDocParts = []
+		Object.keys(methods).forEach(methodName => {
+			const docPart = docFile.reverse().filter(comment => {
+				return (
+					comment.longname.indexOf('methods.' + methodName) > -1 &&
+					listDocParts.indexOf(comment.longname) === -1
+				)
+			})[0]
+			if (docPart) {
+				if (docPart['access'] && docPart['access'] === 'public') {
+					listDocParts.push(docPart.longname)
+					listDocMethods.push(getMethod(methodName, docPart))
+				}
+			}
+		})
+	}
+	return listDocMethods
 }
--- src/utils/processTags.js
+++ src/utils/processTags.js
@@ -2,18 +2,18 @@ import blockTags from './blockTags'
 import getTag from './getTag'
 
 export default function processTags(docPart, ignoreTags = []) {
-  let obj = {}
-  if (docPart) {
-    blockTags
-      .filter(tagName => {
-        return ignoreTags.indexOf(tagName) === -1
-      })
-      .forEach(tagName => {
-        const tag = getTag(tagName, docPart)
-        if (tag) {
-          obj[tagName] = tag
-        }
-      })
-  }
-  return obj
+	let obj = {}
+	if (docPart) {
+		blockTags
+			.filter(tagName => {
+				return ignoreTags.indexOf(tagName) === -1
+			})
+			.forEach(tagName => {
+				const tag = getTag(tagName, docPart)
+				if (tag) {
+					obj[tagName] = tag
+				}
+			})
+	}
+	return obj
 }
