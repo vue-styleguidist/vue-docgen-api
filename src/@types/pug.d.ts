0579e723db3511f5609b9d6d4de4cf96a0a673cb
Bart Ledoux <ledouxb@me.com>
2019-01-06 16:11:02 -0600

    fix npm run compile

--- src/@types/pug.d.ts
+++ src/@types/pug.d.ts
@@ -1,16 +1,3 @@
-declare module 'htmlparser2' {
-  class Parser {
-    constructor(options: {
-      oncomment?: (data: string) => void
-      ontext?: (text: string) => void
-      onopentag?: (name: string, attrs: { name: string }) => void
-    })
-
-    write(template: string): void
-    end(): void
-  }
-}
-
 declare module 'pug-lexer' {
   export class Lexer {
     tokens: any[]
