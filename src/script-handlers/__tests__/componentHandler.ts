46a71977b85923c8396e2ce571fc2b282557ba41
Bart Ledoux <ledouxb@me.com>
2019-01-17 21:44:33 -0600

    fix class style component tag finding

--- src/script-handlers/__tests__/componentHandler.ts
+++ src/script-handlers/__tests__/componentHandler.ts
@@ -1,3 +1,4 @@
+import { ParserPlugin } from '@babel/parser'
 import babylon from '../../babel-parser'
 import { Documentation } from '../../Documentation'
 import resolveExportedComponent from '../../utils/resolveExportedComponent'
@@ -5,8 +6,8 @@ import componentHandler from '../componentHandler'
 
 jest.mock('../../Documentation')
 
-function parse(src: string) {
-  const ast = babylon().parse(src)
+function parse(src: string, plugins: ParserPlugin[] = []) {
+  const ast = babylon({ plugins }).parse(src)
   return resolveExportedComponent(ast)
 }
 
@@ -31,7 +32,7 @@ describe('componentHandler', () => {
     expect(documentation.set).toHaveBeenCalledWith('description', 'An empty component')
   })
 
-  it('should return the right component name', () => {
+  it('should return tags for normal components', () => {
     const src = `
     /**
      * An empty component
@@ -46,6 +47,24 @@ describe('componentHandler', () => {
     componentHandler(documentation, def[0])
     expect(documentation.set).toHaveBeenCalledWith('tags', {
       author: [{ description: '[Rafael]', title: 'author' }],
+      version: [{ description: '12.5.7', title: 'version' }],
+    })
+  })
+
+  it('should return tags for class style components', () => {
+    const src = `
+    /**
+     * An empty component
+     * @version 12.5.7
+     */
+    @Component
+    export default class myComp {
+
+    }
+    `
+    const def = parse(src, ['typescript'])
+    componentHandler(documentation, def[0])
+    expect(documentation.set).toHaveBeenCalledWith('tags', {
       version: [{ description: '12.5.7', title: 'version' }],
     })
   })
--- src/script-handlers/componentHandler.ts
+++ src/script-handlers/componentHandler.ts
@@ -9,6 +9,11 @@ export default function propHandler(documentation: Documentation, path: NodePath
   // in case of Vue.extend() structure
   if (componentCommentedPath.isCallExpression()) {
     componentCommentedPath = componentCommentedPath.parentPath
+  } else if (componentCommentedPath.isDeclaration()) {
+    const classDeclaration = componentCommentedPath.get('declaration')
+    if (!Array.isArray(classDeclaration) && classDeclaration.isClassDeclaration()) {
+      componentCommentedPath = classDeclaration
+    }
   }
   const docBlock = getDocblock(componentCommentedPath)
 
