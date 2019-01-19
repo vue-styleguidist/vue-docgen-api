b5c5723c64cc95b54f9024dfbb0b528eadd37c46
Bart Ledoux <ledouxb@me.com>
2019-01-18 18:26:30 -0600

    add some todo about handlers

--- src/script-handlers/index.ts
+++ src/script-handlers/index.ts
@@ -6,12 +6,14 @@ import displayNameHandler from './displayNameHandler'
 // import eventHandler from './eventHandler'
 import methodHandler from './methodHandler'
 import propHandler from './propHandler'
+// TODO: add render function slots management here
 
 export default [
   componentHandler,
   displayNameHandler,
   methodHandler,
   propHandler,
+  // TODO: uncomment this if @rafaesc is ok with it and fix tests
   // eventHandler,
   classDisplayNameHandler,
   classMethodHandler,
