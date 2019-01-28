17309bd59ba955ad74f151de2aa4add6368eb19b
Bart Ledoux <ledouxb@me.com>
2019-01-28 14:43:28 -0600

    add it to the main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,18 @@ import { parseFile, parseSource as parseSourceLocal } from './parse'
 
 export { ComponentDoc }
 
-export function parse(filePath: string): ComponentDoc {
+export function parse(filePath: string, aliases?: { [alias: string]: string }): ComponentDoc {
   const doc = new Documentation()
-  parseFile(doc, { filePath })
+  parseFile(doc, { filePath, aliases })
   return doc.toObject()
 }
 
-export function parseSource(source: string, filePath: string): ComponentDoc {
+export function parseSource(
+  source: string,
+  filePath: string,
+  aliases?: { [alias: string]: string },
+): ComponentDoc {
   const doc = new Documentation()
-  parseSourceLocal(doc, source, { filePath })
+  parseSourceLocal(doc, source, { filePath, aliases })
   return doc.toObject()
 }
