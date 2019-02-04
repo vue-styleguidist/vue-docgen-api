34a7de62f20237398b9bad99be1f38d9cfaa6e75
Bart Ledoux <ledouxb@me.com>
2019-02-03 18:26:53 -0600

    add resolution of vue files for dependencies

--- src/utils/resolvePathFrom.ts
+++ src/utils/resolvePathFrom.ts
@@ -1,5 +1,18 @@
 export default function resolvePathFrom(path: string, from: string): string {
-  return require.resolve(path, {
-    paths: [from],
-  })
+  let finalPath = ''
+  try {
+    finalPath = require.resolve(path, {
+      paths: [from],
+    })
+  } catch (e) {
+    try {
+      finalPath = require.resolve(`${path}.vue`, {
+        paths: [from],
+      })
+    } catch (e) {
+      throw new Error(`Neither '${path}.vue' nor '${path}.js' could be found in '${from}'`)
+    }
+  }
+
+  return finalPath
 }
