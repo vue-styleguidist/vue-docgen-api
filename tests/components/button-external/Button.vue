b34161c503bb96c135cffe2b06f223420bfa9906
Bart Ledoux <ledouxb@me.com>
2019-02-17 10:58:34 -0600

    test: add external script e2e test

---
+++ tests/components/button-external/Button.vue
@@ -0,0 +1,8 @@
+<template>
+  <button class="buttonComponent" @click.prevent="onClick">
+    <!-- @slot Use this slot default -->
+    <slot></slot>
+  </button>
+</template>
+
+<script src='./button.ts'>
