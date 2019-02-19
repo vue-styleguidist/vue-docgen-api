27389453c5dcb8a0da1a7644fe9f1ccc5b133887
Bart Ledoux <ledouxb@me.com>
2019-02-18 20:09:46 -0600

    extract resolve identifier

--- src/utils/resolveExportedComponent.ts
+++ src/utils/resolveExportedComponent.ts
@@ -4,6 +4,7 @@ import recast from 'recast'
 import Map from 'ts-map'
 import isExportedAssignment from './isExportedAssignment'
 import resolveExportDeclaration from './resolveExportDeclaration'
+import resolveIdentifier from './resolveIdentifier'
 
 function ignore(): boolean {
   return false
@@ -127,51 +128,4 @@ function normalizeComponentPath(path: NodePath): NodePath {
     return path.get('init')
   }
   return path
-}
-
-function resolveIdentifier(ast: bt.File, path: NodePath): NodePath | null {
-  if (!bt.isIdentifier(path.node)) {
-    return path
-  }
-
-  const varName = path.node.name
-  let comp: NodePath | null = null
-
-  recast.visit(ast.program, {
-    // to look only at the root we ignore all traversing
-    visitFunctionDeclaration: ignore,
-    visitFunctionExpression: ignore,
-    visitClassExpression: ignore,
-    visitIfStatement: ignore,
-    visitWithStatement: ignore,
-    visitSwitchStatement: ignore,
-    visitWhileStatement: ignore,
-    visitDoWhileStatement: ignore,
-    visitForStatement: ignore,
-    visitForInStatement: ignore,
-
-    visitVariableDeclaration(variablePath) {
-      if (!bt.isVariableDeclaration(variablePath.node)) {
-        return false
-      }
-      const varID = variablePath.node.declarations[0].id
-      if (!varID || !bt.isIdentifier(varID) || varID.name !== varName) {
-        return false
-      }
-
-      comp = variablePath.get('declarations', 0).get('init')
-      return false
-    },
-
-    visitClassDeclaration(classPath) {
-      const classID = classPath.node.id
-      if (!classID || !bt.isIdentifier(classID) || classID.name !== varName) {
-        return false
-      }
-
-      comp = classPath
-      return false
-    },
-  })
-  return comp
 }
